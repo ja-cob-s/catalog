71fc2621cb456919e97471dab8b3fb81b34a17b0
Jacob Schaible <jnschaible@gmail.com>
2018-12-18 18:56:29 -0500

    Added import for backref

--- database_setup.py
+++ database_setup.py
@@ -3,7 +3,7 @@
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
